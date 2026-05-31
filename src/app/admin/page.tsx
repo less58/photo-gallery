5e2e1375cd1b414601d0cf22360220498cc6b80e
less58 <less58@gmail.com-GitHub>
2026-05-31 22:25:04 +0300

    fix upload2

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,8 @@
 import { createAdminClient } from '@/lib/supabase/admin'
 import AdminPanel from '@/components/AdminPanel'
 
+export const dynamic = 'force-dynamic'
+
 export default async function AdminPage() {
   const supabase = createAdminClient()
 
